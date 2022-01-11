8050fe07826f7c612f2e7e94e423174e52677d54
arminstr <armin.straller@gmail.com>
2022-01-11 18:13:32 +0000

    added loop

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
-
+import time, datetime
+import threading
 import motor
-
 
 HOST = ""
 PORT = 80
 
-currentPos = 42
+"""
+this value has to be changed according to the length of the projector screen and the number of ticks per revolution.
+"""
+MAX_ENCODER_TICKS = 10
+
+currentPos = 0
 targetPos = 0
 
 class Handler(BaseHTTPRequestHandler):
@@ -30,12 +34,32 @@ class Handler(BaseHTTPRequestHandler):
 			currentPos = targetPos
 			self.wfile.write(bytes(str(currentPos), "utf-8"))
 
-motor.init()
-print("motor: initialized...")
+def positionController():
+	"""
+	controls the position of the projector screen.
+	depending on the control difference the motor is turned in the matching direction.
+	in case the end switch is engaged the motor is not allowed to turn into the upwards direction any further.
+	in addition, current position is set to 0.
+	"""
+	global currentPos, targetPos
+
+	# dummy statement
+	print (datetime.datetime.now())
+
+def loop():
+	next_call = time.time()
+	while True:
+		positionController()
+		next_call = next_call+0.1;
+		time.sleep(max(0, next_call - time.time()))
 
 if __name__ == "__main__":
 	motor.init()
 	print("motor: initialized...")
+	timerThread = threading.Thread(target=loop)
+	timerThread.daemon = True
+	timerThread.start()
+	print("loop: initialized...")
 	webServer = HTTPServer((HOST, PORT), Handler)
 	print("server initialized at port", PORT)
 	try:
